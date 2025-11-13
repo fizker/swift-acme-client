5cff5e34c2dc25d4a6a9ccf8158627c70ad9ada4
Benjamin Horsleben <benjamin@fizkerinc.dk>
2025-11-13 21:52:24 +0100

    Updated AccountCommand to use the API to fetch the URL for an account

--- Sources/ACMEClientCLI/AccountCommand.swift
+++ Sources/ACMEClientCLI/AccountCommand.swift
@@ -1,8 +1,5 @@
 import ACMEClient
-import ACMEClientModels
 import ArgumentParser
-import Foundation
-import FzkExtensions
 
 struct AccountCommand: AsyncParsableCommand {
 	static let configuration = CommandConfiguration(
@@ -13,51 +10,14 @@ struct AccountCommand: AsyncParsableCommand {
 	@Option(name: .shortAndLong)
 	var directory: Directory
 
+	@Option(name: .shortAndLong, transform: { try .init(pemRepresentation: $0) })
+	var accountKey: Key.Private
+
 	func run() async throws {
 		print("fetching account now: \(directory), \(directory.acme), \(directory.acme.rawValue)")
-		let client = try await ACMEClient(directory: directory.acme)
-		let account = try await client.account()
-		print("Account: \(account)")
+		let api = try await API(directory: directory.acme)
+		var nonce = try await api.fetchNonce()
+		let accountURL = try await api.fetchAccountURL(nonce: &nonce, accountKey: accountKey)
+		print("Account: \(accountURL, default: "No URL returned")")
 	}
-}
-
-enum Directory {
-	case letsEncryptProduction, letsEncryptStaging
-	case custom(URL)
-
-	var acme: ACMEDirectory {
-		switch self {
-		case .letsEncryptProduction: .letsEncryptV2Production
-		case .letsEncryptStaging: .letsEncryptV2Staging
-		case let .custom(url): .custom(url)
-		}
-	}
-}
-extension Directory: CaseIterable {
-	static var allCases: [Directory] { [.letsEncryptProduction, .letsEncryptStaging] }
-}
-extension Directory: ExpressibleByArgument {
-	enum Error: Swift.Error {
-		case supportedShortHands([String])
-	}
-	init?(argument: String) {
-		switch argument {
-		case "lets-encrypt-production": self = .letsEncryptProduction
-		case "lets-encrypt-staging": self = .letsEncryptStaging
-		default:
-			guard argument.starts(with: "http")
-			else {
-				return nil
-			}
-
-			guard let url = URL(string: argument)
-			else { return nil }
-			self = .custom(url)
-		}
-	}
-
-	static var allValueStrings: [String] { [
-		"lets-encrypt-production",
-		"lets-encrypt-staging",
-	] }
 }
