de6a4673ddaaca55923926d4d1d7db6270ffbd77
Benjamin Horsleben <benjamin@fizkerinc.dk>
2025-12-21 19:42:13 +0100

    Simplified the Challenge type after finding the documentation in the RFC

--- Sources/ACMEAPIModels/Challenge.swift
+++ Sources/ACMEAPIModels/Challenge.swift
@@ -1,11 +1,104 @@
-public enum Challenge: Equatable, Sendable {
-	case unknown(GenericChallenge)
-	case letsEncrypt(LetsEncryptChallenge)
+public import Foundation
 
-	var status: Status {
-		switch self {
-		case let .unknown(value): return value.status
-		case let .letsEncrypt(value): return value.status
+/// An ACME challenge object represents a server's offer to validate a
+/// client's possession of an identifier in a specific way.  Unlike the
+/// other objects listed above, there is not a single standard structure
+/// for a challenge object.  The contents of a challenge object depend on
+/// the validation method being used.  The general structure of challenge
+/// objects and an initial set of validation methods are described in
+/// [Section 8](https://datatracker.ietf.org/doc/html/rfc8555#section-8).
+public struct Challenge: Codable, Equatable, Sendable {
+	/// Status of the challenge.
+	///
+	/// Challenge objects are created in the "pending" state.  They
+	/// transition to the "processing" state when the client responds to the
+	/// challenge (see Section 7.5.1) and the server begins attempting to
+	/// validate that the client has completed the challenge.  Note that
+	/// within the "processing" state, the server may attempt to validate the
+	/// challenge multiple times (see Section 8.2).  Likewise, client
+	/// requests for retries do not cause a state change.  If validation is
+	/// successful, the challenge moves to the "valid" state; if there is an
+	/// error, the challenge moves to the "invalid" state.
+	///
+	/// ### State Transitions for Challenge Objects
+	/// ```
+	///          pending
+	///             |
+	///             | Receive
+	///             | response
+	///             V
+	///         processing <-+
+	///             |   |    | Server retry or
+	///             |   |    | client retry request
+	///             |   +----+
+	///             |
+	///             |
+	/// Successful  |   Failed
+	/// validation  |   validation
+	///   +---------+---------+
+	///   |                   |
+	///   V                   V
+	/// valid              invalid
+	/// ```
+	public var status: Status
+
+	/// The type of challenge.
+	public var type: `Type`
+	public var url: URL
+	public var token: String
+
+	public init(status: Status, token: String, type: Type, url: URL) {
+		self.status = status
+		self.type = type
+		self.url = url
+		self.token = token
+	}
+
+	/// https://letsencrypt.org/docs/challenge-types/
+	public enum `Type`: Codable, Equatable, RawRepresentable, Sendable {
+		/// https://datatracker.ietf.org/doc/html/rfc8737
+		/// https://letsencrypt.org/docs/challenge-types/#tls-alpn-01
+		case tlsALPN
+		/// https://datatracker.ietf.org/doc/html/rfc8555#section-8.4
+		/// https://letsencrypt.org/docs/challenge-types/#dns-01-challenge
+		case dns
+		/// https://datatracker.ietf.org/doc/html/rfc8555#section-8.3
+		/// https://letsencrypt.org/docs/challenge-types/#http-01-challenge
+		case http
+
+		/// Catch-all for unknown types
+		case unknown(String)
+
+		public init?(rawValue: String) {
+			switch rawValue {
+			case "tls-alpn-01": self = .tlsALPN
+			case "dns-01": self = .dns
+			case "http-01": self = .http
+			default: self = .unknown(rawValue)
+			}
+		}
+
+		public var rawValue: String {
+			switch self {
+			case .tlsALPN: "tls-alpn-01"
+			case .dns: "dns-01"
+			case .http: "http-01"
+			case let .unknown(rawValue): rawValue
+			}
+		}
+
+		public init(from decoder: any Decoder) throws {
+			let container = try decoder.singleValueContainer()
+			let value = try container.decode(String.self)
+			guard let supportedType = Self(rawValue: value)
+			else {
+				throw DecodingError.dataCorruptedError(
+					in: container,
+					debugDescription: "Unsupported challenge type: \(value)"
+				)
+			}
+
+			self = supportedType
 		}
 	}
 
@@ -14,32 +107,13 @@ public enum Challenge: Equatable, Sendable {
 	}
 }
 
-extension Challenge: Codable {
-	public init(from decoder: any Decoder) throws {
-		if let le = try? LetsEncryptChallenge(from: decoder) {
-			self = .letsEncrypt(le)
-		} else {
-			self = try .unknown(.init(from: decoder))
-		}
-	}
-
-	public func encode(to encoder: any Encoder) throws {
-		switch self {
-		case let .letsEncrypt(value):
-			try value.encode(to: encoder)
-		case let .unknown(value):
-			try value.encode(to: encoder)
-		}
-	}
-}
-
 extension Challenge: CustomStringConvertible {
 	public var description: String {
-		switch self {
-		case let .letsEncrypt(value):
-			return value.description
-		case let .unknown(value):
-			return value.description
-		}
+		"""
+		status: "\(status)"
+		token: "\(token)"
+		type: "\(type.rawValue)"
+		url: "\(url)"
+		"""
 	}
 }
--- Tests/ACMEAPIModelsTests/ChallengeTests.swift
+++ Tests/ACMEAPIModelsTests/ChallengeTests.swift
@@ -1,68 +1,119 @@
 import ACMEAPIModels
 import CompileSafeInitMacro
 import Foundation
+import FzkExtensions
+import SwifterJSON
 import Testing
 
 struct ChallengeTests {
-	@Test
-	func decodeFromJSON__multipleChallengeTypes__decodesCorrectly() async throws {
-		let json = """
-		[
-			{
-				"status": "pending",
-				"foo": 1,
-				"bar": "baz",
-				"array": [ 1, true, "foobar" ]
-			},
-			{
-				"status": "pending",
-				"token": "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
-				"type": "http-01",
-				"url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/z6xfyg"
-			}
-		]
-		"""
+	@Test(arguments: testData.map { ($0.json, $0.challenge) })
+	func decodeFromJSON__decodesCorrectly(json: String, expected: Challenge) async throws {
+		let actual = try decode(json) as Challenge
+		#expect(actual == expected)
+	}
 
-		let actual = try decode(json) as [Challenge]
+	@Test(arguments: testData.map { ($0.challenge, $0.description) })
+	func description__outputIsFormatted(challenge: Challenge, expected: String) async throws {
+		let actual = """
+			CH:
+			\(challenge, indentedWith: "  ")
+			"""
 
-		#expect(actual == [
-			.unknown(.init(
-				status: .pending,
-				allFields: [
-					"status": "pending",
-					"foo": 1,
-					"bar": "baz",
-					"array": [ 1, true, "foobar" ],
-				],
-			)),
-			.letsEncrypt(.init(
+		#expect(actual == expected)
+	}
+
+	static let testData: [(challenge: Challenge, description: String, json: String)] = [
+		(
+			Challenge(
 				status: .pending,
 				token: "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
 				type: .http,
 				url: #URL("https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/z6xfyg"),
-			))
-		])
-	}
-
-	@Test
-	func description__returnsTheInnerDescription() async throws {
-		let generic = GenericChallenge(
-			status: .processing,
-			allFields: [
-				"status": "processing",
-				"foo": 1,
-				"bar": "baz",
-				"array": [ 1, true, "foobar" ],
-			],
-		)
-		#expect("\(Challenge.unknown(generic))" == "\(generic)")
-
-		let letsEncrypt = LetsEncryptChallenge(
-			status: .valid,
-			token: "foo",
-			type: .dns,
-			url: #URL("http://example.com"),
-		)
-		#expect("\(Challenge.letsEncrypt(letsEncrypt))" == "\(letsEncrypt)")
-	}
+			),
+			"""
+			CH:
+			  status: "pending"
+			  token: "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw"
+			  type: "http-01"
+			  url: "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/z6xfyg"
+			""",
+			"""
+			{
+			    "status": "pending",
+			    "token": "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
+			    "type": "http-01",
+			    "url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/z6xfyg"
+			}
+			""",
+		),
+		(
+			Challenge(
+				status: .pending,
+				token: "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
+				type: .dns,
+				url: #URL("https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/_oYs2Q"),
+			),
+			"""
+			CH:
+			  status: "pending"
+			  token: "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw"
+			  type: "dns-01"
+			  url: "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/_oYs2Q"
+			""",
+			"""
+			{
+			  "status": "pending",
+			  "token": "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
+			  "type": "dns-01",
+			  "url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/_oYs2Q"
+			}
+			""",
+		),
+		(
+			Challenge(
+				status: .pending,
+				token: "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
+				type: .tlsALPN,
+				url: #URL("https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/fQZpoA"),
+			),
+			"""
+			CH:
+			  status: "pending"
+			  token: "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw"
+			  type: "tls-alpn-01"
+			  url: "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/fQZpoA"
+			""",
+			"""
+			{
+			  "status": "pending",
+			  "token": "X7l4LFRh_GRb4Nrq_Qum5i8kUP5ej1foHtPd9AmdaNw",
+			  "type": "tls-alpn-01",
+			  "url": "https://acme-staging-v02.api.letsencrypt.org/acme/chall/243411833/20483825683/fQZpoA"
+			}
+			""",
+		),
+		(
+			Challenge(
+				status: .pending,
+				token: "foo",
+				type: .unknown("foobar"),
+				url: #URL("http://example.com"),
+			),
+			"""
+			CH:
+			  status: "pending"
+			  token: "foo"
+			  type: "foobar"
+			  url: "http://example.com"
+			""",
+			"""
+			{
+			  "status": "pending",
+			  "token": "foo",
+			  "type": "foobar",
+			  "url": "http://example.com"
+			}
+			""",
+		),
+	]
 }
