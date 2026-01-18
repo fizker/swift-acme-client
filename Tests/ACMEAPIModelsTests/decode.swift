905a8c4353252e37bd3edf626a6c7a185d919993
Benjamin Horsleben <benjamin@fizkerinc.dk>
2026-01-18 15:41:49 +0100

    Fixed an error with encoding EmailURL

--- Tests/ACMEAPIModelsTests/decode.swift
+++ Tests/ACMEAPIModelsTests/decode.swift
@@ -1,6 +1,12 @@
+import ACMEAPIModels
 import Foundation
 
 func decode<T: Decodable>(_ json: String) throws -> T {
 	let data = json.data(using: .utf8)!
-	return try JSONDecoder().decode(T.self, from: data)
+	return try apiCoder.decode(data)
+}
+
+func encode(_ encodable: some Encodable) throws -> String {
+	let data = try apiCoder.encode(encodable)
+	return String(data: data, encoding: .utf8)!
 }
